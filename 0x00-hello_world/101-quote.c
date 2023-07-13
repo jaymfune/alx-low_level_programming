6e6da33c49175e07b686384ecd773a4fb2295eda
jaymfune <jacobmfune2@gmail.com>
2023-07-13 05:48:06 -0700

    C program to print error message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print an error message
+ *
+ * Return: Return 1 to symbolise an error.
+*/
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
