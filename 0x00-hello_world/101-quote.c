f7bf782cf99635bd8d5c29e9818dfd02cfa6e400
jaymfune <jacobmfune2@gmail.com>
2023-07-13 05:55:27 -0700

    C program to print error message, fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,8 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", msg);
 	return (1);
 }
