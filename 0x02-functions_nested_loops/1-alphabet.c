36415a97de9c9f274a4acfb7676ad29e53e13723
jaymfune <jacobmfune2@gmail.com>
2023-07-18 04:34:42 -0700

    C program to print the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
+#include"main.h"
 
+/**
+ * print_alphabet - print alphabet
+*/
+
+void print_alphabet()
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
