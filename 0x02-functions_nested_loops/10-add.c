a207c3e8794d1ed5d48afb6235b4c2de307f2a0d
jaymfune <jacobmfune2@gmail.com>
2023-07-18 11:48:59 -0700

    C program to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @x: first input
+ * @y: second input
+ *
+ * Return: sum of x & y
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
