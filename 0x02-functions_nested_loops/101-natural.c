11147b5c693f09eda14c6678c57d6442efd5f714
jaymfune <jacobmfune2@gmail.com>
2023-07-19 10:01:28 -0700

    C program to compute the sum of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: computes the sum of all
+ *		the multiples of 3 or 5
+ *		below 1024 (excluded)
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+
+	printf("%i", sum);
+
+	return (0);
+}
