2be9a6114d8637c9e8333c78fe9b74c9e7a8e78f
jaymfune <jacobmfune2@gmail.com>
2023-07-20 13:27:20 -0700

    C program to print a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle
+ * @size: sets the size of the triangle
+*/
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	/* Size to determines loop count */
+	for (i = 0; i <= size; i++)
+	{
+		/* Print space if the i if greater than 0 */
+		if (i > 0)
+		{
+			for (k = 0; k < size - i; k++)
+			{
+				printf(" ");
+			}
+		}
+
+		/* Print # */
+		for (j = 0; j < i; j++)
+		{
+			printf("#");
+		}
+
+		/* Start new line if i if greater than 0 */
+		if (i > 0)
+		{
+			printf("\n");
+		}
+	}
+}
