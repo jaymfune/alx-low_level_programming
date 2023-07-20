069d8131233ff84d70cd1dfb8762b3be3bed6877
jaymfune <jacobmfune2@gmail.com>
2023-07-20 09:26:01 -0700

    C program to print numbers from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i <= 9; i++)
+	{
+		_putchar(i + 48);
+	}
+
+	_putchar('\n');
+}
