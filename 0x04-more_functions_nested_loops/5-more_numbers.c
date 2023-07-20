41bbcfef7b5c29b66f2121ca0ec101fc2fb8fc08
jaymfune <jacobmfune2@gmail.com>
2023-07-20 11:01:27 -0700

    C program that prints numbers 0 - 14 x10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ * putchar only 3 times
+ * Return: 0-14 x10 followed by new line
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				putchar((j / 10) + '0');
+			}
+			putchar((j % 10) + '0');
+		}
+
+		putchar('\n');
+	}
+}
