ec6d15ec6f63f12af81089de7b40c4b7db394936
jaymfune <jacobmfune2@gmail.com>
2023-07-24 09:59:28 -0700

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,7 +15,7 @@ void print_rev(char *s)
 		length++;
 	}
 
-	// Print the characters in reverse order
+	/* Print the characters in reverse order */
 	for (int i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
