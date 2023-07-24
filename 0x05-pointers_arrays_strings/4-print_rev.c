c6848a29147014967d938e9cada4627781a054dc
jaymfune <jacobmfune2@gmail.com>
2023-07-24 10:00:52 -0700

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,21 +4,22 @@
  * print_rev - prints a string in reverse, followed by a new line
  * @s: string to be printed
  */
-
 void print_rev(char *s)
 {
-	/* Find the length of the string */
-	int length = 0;
+	int i, j, len;
 
-	while (s[length] != '\0')
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		length++;
+		i++;
 	}
 
-	/* Print the characters in reverse order */
-	for (int i = length - 1; i >= 0; i--)
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[i]);
+		_putchar(s[j]);
 	}
 
 	_putchar('\n');
