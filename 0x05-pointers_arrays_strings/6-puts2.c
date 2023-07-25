4cb247fe1a4d609ec9552e21c21612fba707608a
jaymfune <jacobmfune2@gmail.com>
2023-07-24 21:58:11 -0700

    print every other character in a string, fix bug

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,14 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, length;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length; i += 2)
 	{
 		_putchar(str[i]);
 	}
