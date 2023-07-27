d1bc2dc8513a9a13ea87215acf5422d1ea6bf4c8
jaymfune <jacobmfune2@gmail.com>
2023-07-27 03:43:15 -0700

    concat two strings until n characters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	/* Find the end of the dest string */
+	char *dest_end = dest;
+
+	while (*dest_end != '\0')
+	{
+		dest_end++;
+	}
+
+	/* Concat the src string to the dest string, up to n characters */
+	int = 0;
+	while (*src != '\0' && i < n)
+	{
+		*dest_end = *src;
+		dest_end++;
+		src++;
+		i++;
+	}
+
+	/* Add a terminating null byte to the end of the concatenated string */
+	*dest_end = '\0';
+
+	return (dest);
+}
