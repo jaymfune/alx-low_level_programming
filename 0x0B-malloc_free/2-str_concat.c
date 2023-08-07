120e539f9711c147472d8b231de295ef341f959e
jaymfune <jacobmfune2@gmail.com>
2023-08-07 09:46:02 -0700

    concat two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int len1, len2, i;
+	char *s3;
 
 
 	/* Handle cases where both input strings are empty. */
 	if (s1 == NULL && s2 == NULL)
-		return NULL;
+		return (NULL);
 
 
 	/* Calculate lengths of input strings. */
@@ -25,7 +26,7 @@ char *str_concat(char *s1, char *s2)
 	len2 = (s2 ? strlen(s2) : 0);
 
 	/* Allocate memory for concatenated string. */
-	char *s3 = (char *)malloc(len1 + len2 + 1);
+	s3 = (char *)malloc(len1 + len2 + 1);
 	if (s3 == NULL)
 		return (NULL);
 
@@ -44,6 +45,6 @@ char *str_concat(char *s1, char *s2)
 	/* Null-terminate the concatenated string. */
 	s3[len1 + len2] = '\0';
 
-	return s3;
+	return (s3);
 }
 
