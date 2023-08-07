abc0b43de7116497f6b18e7ff964065f75affa36
jaymfune <jacobmfune2@gmail.com>
2023-08-07 08:30:00 -0700

    create copy of a string, fix error

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,14 @@
 
 char *_strdup(char *str)
 {
+	char *dup;
+
 	/* Check if the input string is NULL. */
 	if (str == NULL)
 		return (NULL);
 
 	/* Allocate memory for the duplicate string, including space for the null terminator. */
-	char *dup = (char *)malloc(strlen(str) + 1);
+	dup = (char *)malloc(strlen(str) + 1);
 
 	/* Check if memory allocation was successful. */
 	if (dup == NULL)
