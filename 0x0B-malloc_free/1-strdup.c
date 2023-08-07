91a91f81a01a1166ebcbfaa3d9a2800b1370027e
jaymfune <jacobmfune2@gmail.com>
2023-08-07 08:22:58 -0700

    create copy of a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+
+	/* Check if the input string is NULL. */
+	if (str == NULL)
+		return (NULL);
+
+	/* Allocate memory for the duplicate string, including space for the null terminator. */
+	char *dup = (char *)malloc(strlen(str) + 1);
+
+	/* Check if memory allocation was successful. */
+	if (dup == NULL)
+		return (NULL);
+
+	/* Copy the content of the input string 'str' into the allocated memory 'dup'. */
+	strcpy(dup, str);
+
+	/* Return the pointer to the newly created duplicate string. */
+	return (dup);
+}
+
+
