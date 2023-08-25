6a8ec1b800a6ec66ef497aff0fadbb55a493cf54
jaymfune <jacobmfune2@gmail.com>
2023-08-25 00:05:20 -0700

    fix of task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,33 +6,25 @@
   * Return: Number of nodes
   */
 
+
 size_t print_list(const list_t *h)
 {
 	/* counter for number of nodes */
 	size_t node_count = 0;
 
-	/* If the input pointer is NULL (empty list), return 0. */
-	if (h == NULL)
-		return (0);
-
-	/* Loop through the linked list until the last node is reached. */
-	while (h->str == NULL)
+	while (h)
 	{
 		/* If the string in the current node is NULL, print [0] (nil). */
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			/* Print the length and content of the string in the current node. */
 			printf("[%u] %s\n", h->len, h->str);
-
 		/* Move to the next node in the list. */
 		h = h->next;
 
 		node_count++;
 	}
-
-	/* Print information for the last node. */
-	printf("[%d] %s\n", h->len, h->str);
 
 	return (node_count);
 }
