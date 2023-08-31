a0dde82d85c4dbe6edab4dab3dcb881f22febc7b
jaymfune <jacobmfune2@gmail.com>
2023-08-31 08:28:39 -0700

    converts a binary number to an unsigned int

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -3,9 +3,10 @@
 /**
   * binary_to_uint - converts a binary number to unsigned int
   * @b: string containing the binary number
-  * Return: the converted number
+  * Return: the conv(const char *b)
   */
-unsigned int binary_to_unit(const char *b)
+
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
